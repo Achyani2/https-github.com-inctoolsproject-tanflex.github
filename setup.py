4ce3273b183e5e624255433f3592bfa36f06708a
Pratan naimjoi <52064201+tanpattaya@users.noreply.github.com>
2019-10-21 02:29:14 +0700

    Update and rename save setup.py to setup.py

---
+++ setup.py
@@ -0,0 +1,65 @@
+from linepy import *
+from liff.ttypes import LiffChatContext, LiffContext, LiffSquareChatContext, LiffNoneContext, LiffViewRequest
+from akad.ttypes import Message
+from akad.ttypes import ContentType as Type
+from akad.ttypes import TalkException
+from datetime import datetime, timedelta
+from time import sleep
+from bs4 import BeautifulSoup as bSoup
+from bs4 import BeautifulSoup
+from humanfriendly import format_timespan, format_size, format_number, format_length
+from gtts import gTTS
+from threading import Thread
+from io import StringIO
+from multiprocessing import Pool
+from googletrans import Translator
+from urllib.parse import urlencode
+from tmp.MySplit import *
+from random import randint
+from shutil import copyfile
+from youtube_dl import YoutubeDL
+import subprocess, youtube_dl, humanize, traceback
+import subprocess as cmd
+import platform
+import requests, json
+import time, random, sys, json, null, pafy, codecs, html5lib ,shutil ,threading, glob, re, base64, string, os, requests, six, ast, pytz, wikipedia, urllib, urllib.parse, atexit, asyncio, traceback
+_session = requests.session()
+try:
+    import urllib.request as urllib2
+except ImportError:
+    import urllib2
+#==============================================================================#
+_session = requests.session()
+botStart = time.time()
+print("LOGIN                        INDUK")
+print("login success")
+oepoll = OEPoll(cl)
+call = cl
+cl= LINE()
+cl.log("Auth Token : " + str(cl.authToken))
+cl.log("Timeline Token : " + str(cl.tl.channelAccessToken))
+waitOpen = codecs.open("cl.json","r","utf-8")
+settingsOpen = codecs.open("cl2","r","utf-8")
+imagesOpen = codecs.open("image.json","r","utf-8")
+stickersOpen = codecs.open("sticker.json","r","utf-8")
+wait = json.load(waitOpen)
+images = json.load(imagesOpen)
+settings = json.load(settingsOpen)
+stickers = json.load(stickersOpen)
+clMID = cl.profile.mid
+clProfile = cl.getProfile()
+clSettings = cl.getSettings()
+clPoll = OEPoll(cl)
+clMID = cl.getProfile().mid
+admin = [clMID]
+loop = asyncio.get_event_loop()
+listToken = ['desktopmac','desktopwin','iosipad','chromeos','win10']
+mc = {"wr":{}}
+unsendchat = {}
+msgdikirim = {}
+msg_image={}
+msg_video={}
+msg_sticker={}
+wbanlist = []
+msg_dict = {}
+temp_flood = {}
