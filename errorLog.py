eca58ab6e18e112cdfd3321076ad1424adac062e
Pratan naimjoi <52064201+tanpattaya@users.noreply.github.com>
2019-10-21 10:45:23 +0700

    Update errorLog.py

--- errorLog.py
+++ errorLog.py
@@ -53,7 +53,151 @@ window.gon={};gon.api_version="v4";gon.default_avatar_url="https://assets.github
 <link color="rgb(226, 67, 41)" href="https://assets.github-static.net/assets/logo-d36b5212042cebc89b96df4bf6ac24e43db316143e89926c0db839ff694d2de4.svg" rel="mask-icon">
 <meta content="https://assets.github-static.net/assets/msapplication-tile-1196ec67452f618d39cdd85e2e3a542f76574c071051ae7effbfde01710eb17d.png" name="msapplication-TileImage">
 <meta content="#30353E" name="msapplication-TileColor">
+   
+    <link rel="dns-prefetch" href="https://github.githubassets.com">
+  <link rel="dns-prefetch" href="https://avatars0.githubusercontent.com">
+  <link rel="dns-prefetch" href="https://avatars1.githubusercontent.com">
+  <link rel="dns-prefetch" href="https://avatars2.githubusercontent.com">
+  <link rel="dns-prefetch" href="https://avatars3.githubusercontent.com">
+  <link rel="dns-prefetch" href="https://github-cloud.s3.amazonaws.com">
+  <link rel="dns-prefetch" href="https://user-images.githubusercontent.com/">
 
+
+
+  <link crossorigin="anonymous" media="all" integrity="sha512-L6knYQj0qk2lhbBdXyy/eF0pc3uMue04cNJLLNWlHboKkkUQLJd4JbdYHGc9aqy92/vfVBLnddS6bpVf285Pqg==" rel="stylesheet" href="https://github.githubassets.com/assets/frameworks-c21c347ce575eb3ded7531f19af1d324.css" />
+  
+    <link crossorigin="anonymous" media="all" integrity="sha512-Eg6fSzYLua+U8QHG4DPY7oNAT6Ohj0BpQ9CTDa2Yzlc2MrXBBGM9iuAXE3g3WGCaFG+2ngLVh2qhQMril+4vOg==" rel="stylesheet" href="https://github.githubassets.com/assets/github-89d9d3c9d19ddf234d9355a9ff50bcbd.css" />
+    
+    
+    
+    
+
+  <meta name="viewport" content="width=device-width">
+  
+  <title>tanflex.github.io/changes.atom at master · tanpattaya/tanflex</title>
+    <meta name="description" content="Messaging APIs for multi-platform/tanflex.github.io&#39;s private - tanpattaya/tanflex">
+    <link rel="search" type="application/opensearchdescription+xml" href="/opensearch.xml" title="GitHub">
+  <link rel="fluid-icon" href="https://github.com/fluidicon.png" title="GitHub">
+  <meta property="fb:app_id" content="1401488693436528">
+
+    <meta name="twitter:image:src" content="https://avatars0.githubusercontent.com/u/52064201?s=400&amp;v=4" /><meta name="twitter:site" content="@github" /><meta name="twitter:card" content="summary" /><meta name="twitter:title" content="tanpattaya/linepy" /><meta name="twitter:description" content="Messaging APIs for multi-platform/LINE Python -- LINE Messaging&#39;s private API - tanpattaya/linepy" />
+    <meta property="og:image" content="https://avatars0.githubusercontent.com/u/52064201?s=400&amp;v=4" /><meta property="og:site_name" content="GitHub" /><meta property="og:type" content="object" /><meta property="og:title" content="tanpattaya/linepy" /><meta property="og:url" content="https://github.com/tanpattaya/linepy" /><meta property="og:description" content="Messaging APIs for multi-platform/LINE Python -- LINE Messaging&#39;s private API - tanpattaya/linepy" />
+
+  <link rel="assets" href="https://github.githubassets.com/">
+  <link rel="web-socket" href="wss://live.github.com/_sockets/VjI6NDQxNTI5MjUzOmQ3NzZlNDA3ODZmNjZkNTYyNTFjMjI0ZTFiYjY0NWM4MTVlNDI3YzZiM2VlYjdjZjM0ZTM1NTkzMzFhMDdjNGY=--40c0b18f4e8d61e0bf0bc5c772f4afa5731bc623">
+  <link rel="sudo-modal" href="/sessions/sudo_modal">
+  <meta name="request-id" content="B804:6079:176D60C:21EBD5D:5D9B38BB" data-pjax-transient>
+
+   <meta name="selected-link" value="repo_source" data-pjax-transient>
+
+      <meta name="google-site-verification" content="KT5gs8h0wvaagLKAVWq8bbeNwnZZK1r1XQysX3xurLU">
+    <meta name="google-site-verification" content="ZzhVyEFwb7w3e0-uOTltm8Jsck2F5StVihD0exw2fsA">
+    <meta name="google-site-verification" content="GXs5KoUUkNCoaAZn7wPN-t01Pywp9M3sEjnt_3_ZWPc">
+
+  <meta name="octolytics-host" content="collector.githubapp.com" /><meta name="octolytics-app-id" content="github" /><meta name="octolytics-event-url" content="https://collector.githubapp.com/github-external/browser_event" /><meta name="octolytics-dimension-request_id" content="B804:6079:176D60C:21EBD5D:5D9B38BB" /><meta name="octolytics-dimension-region_edge" content="ap-southeast-1" /><meta name="octolytics-dimension-region_render" content="iad" /><meta name="octolytics-dimension-ga_id" content="" class="js-octo-ga-id" /><meta name="octolytics-dimension-visitor_id" content="7492215869818542630" /><meta name="octolytics-actor-id" content="52064201" /><meta name="octolytics-actor-login" content="tanpattaya" /><meta name="octolytics-actor-hash" content="7e2465c230c6dd6b783aa4dacb81191dcdf8c83b4a3a27887b368af1008856af" />
+<meta name="analytics-location" content="/&lt;user-name&gt;/&lt;repo-name&gt;/blob/show" data-pjax-transient="true" />
+
+
+
+    <meta name="google-analytics" content="UA-3769691-2">
+
+  <meta class="js-ga-set" name="userId" content="c75ecaff14f63a691f129756b080e0fd">
+
+<meta class="js-ga-set" name="dimension1" content="Logged In">
+
+  <meta class="js-ga-set" name="dimension3" content="mobile">
+
+
+  
+
+      <meta name="hostname" content="github.com">
+    <meta name="user-login" content="tanpattaya">
+
+      <meta name="expected-hostname" content="github.com">
+    <meta name="js-proxy-site-detection-payload" content="MDY2OWY1ZDA5ZTIwMzNkNzJiN2I3ZWI5NmQxMWIzNzBlMzZmNmM3ZDg5NGM1Yjc0YmViODBmZTZiNTYxNjFmOHx7InJlbW90ZV9hZGRyZXNzIjoiNDkuNDguNDUuODgiLCJyZXF1ZXN0X2lkIjoiQjgwNDo2MDc5OjE3NkQ2MEM6MjFFQkQ1RDo1RDlCMzhCQiIsInRpbWVzdGFtcCI6MTU3MDQ1MzcwMSwiaG9zdCI6ImdpdGh1Yi5jb20ifQ==">
+
+    <meta name="enabled-features" content="ACTIONS_V2_ON_MARKETPLACE,MARKETPLACE_FEATURED_BLOG_POSTS,MARKETPLACE_INVOICED_BILLING,MARKETPLACE_SOCIAL_PROOF_CUSTOMERS,MARKETPLACE_TRENDING_SOCIAL_PROOF,MARKETPLACE_RECOMMENDATIONS,MARKETPLACE_PENDING_INSTALLATIONS,NOTIFY_ON_BLOCK,RELATED_ISSUES,GHE_CLOUD_TRIAL">
+
+  <meta name="html-safe-nonce" content="08e0a47a767c7775d50cf3443dfb97e1e8e19b0e">
+
+  <meta http-equiv="x-pjax-version" content="40f3e3df870ebd2145fdc19802b159c8">
+  
+
+      <link href="https://github.com/tanpattaya/tanflex.github.io/commits/master.atom" rel="alternate" title="Recent Commits to tanflex.github.io:master" type="application/atom+xml">
+
+  <meta name="go-import" content="github.com/tanpattaya/tanflex.github.io git https://github.com/tanpattaya/tanflex.github.io.git">
+
+  <meta name="octolytics-dimension-user_id" content="52064201" /><meta name="octolytics-dimension-user_login" content="tanpattaya" /><meta name="octolytics-dimension-repository_id" content="212721722" /><meta name="octolytics-dimension-repository_nwo" content="tanpattaya/tanflex.github.io" /><meta name="octolytics-dimension-repository_public" content="true" /><meta name="octolytics-dimension-repository_is_fork" content="false" /><meta name="octolytics-dimension-repository_network_root_id" content="212721722" /><meta name="octolytics-dimension-repository_network_root_nwo" content="tanpattaya/tanflex.github.io" /><meta name="octolytics-dimension-repository_explore_github_marketplace_ci_cta_shown" content="false" />
+
+
+    <link rel="canonical" href="https://github.com/tanpattaya/tanflex.github.io/blob/master/changes.atom" data-pjax-transient>
+
+
+  <meta name="browser-stats-url" content="https://api.github.com/_private/browser/stats">
+
+  <meta name="browser-errors-url" content="https://api.github.com/_private/browser/errors">
+
+  <link rel="mask-icon" href="https://github.githubassets.com/pinned-octocat.svg" color="#000000">
+  <link rel="icon" type="image/x-icon" class="js-site-favicon" href="https://github.githubassets.com/favicon.ico">
+
+<meta name="theme-color" content="#1e2327">
+
+
+
+  <meta name="webauthn-auth-enabled" content="true">
+
+  <meta name="webauthn-registration-enabled" content="true">
+
+  <link rel="manifest" href="/manifest.json" crossOrigin="use-credentials">
+
+  </head>
+
+  <body class="logged-in env-production page-responsive page-blob">
+    
+
+  <div class="position-relative js-header-wrapper ">
+    <a href="#start-of-content" tabindex="1" class="p-3 bg-blue text-white show-on-focus js-skip-to-content">Skip to content</a>
+    <span class="Progress progress-pjax-loader position-fixed width-full js-pjax-loader-bar">
+      <span class="progress-pjax-loader-bar top-0 left-0" style="width: 0%;"></span>
+    </span>
+
+    
+      <header class="Header js-details-container Details flex-wrap flex-lg-nowrap p-responsive" role="banner">
+
+    <div class="Header-item d-none d-lg-flex">
+      <a class="Header-link" href="https://github.com/" data-hotkey="g d" aria-label="Homepage" data-ga-click="Header, go to dashboard, icon:logo">
+  <svg class="octicon octicon-mark-github v-align-middle" height="32" viewBox="0 0 16 16" version="1.1" width="32" aria-hidden="true"><path fill-rule="evenodd" d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0 0 16 8c0-4.42-3.58-8-8-8z"/></svg>
+</a>
+
+    </div>
+
+    <div class="Header-item d-lg-none">
+      <button class="Header-link btn-link js-details-target" type="button" aria-label="Toggle navigation" aria-expanded="false">
+        <svg height="24" class="octicon octicon-three-bars" viewBox="0 0 12 16" version="1.1" width="18" aria-hidden="true"><path fill-rule="evenodd" d="M11.41 9H.59C0 9 0 8.59 0 8c0-.59 0-1 .59-1H11.4c.59 0 .59.41.59 1 0 .59 0 1-.59 1h.01zm0-4H.59C0 5 0 4.59 0 4c0-.59 0-1 .59-1H11.4c.59 0 .59.41.59 1 0 .59 0 1-.59 1h.01zM.59 11H11.4c.59 0 .59.41.59 1 0 .59 0 1-.59 1H.59C0 13 0 12.59 0 12c0-.59 0-1 .59-1z"/></svg>
+      </button>
+    </div>
+
+    <div class="Header-item Header-item--full flex-column flex-lg-row width-full flex-order-2 flex-lg-order-none mr-0 mr-lg-3 mt-3 mt-lg-0 Details-content--hidden">
+        <div class="header-search flex-self-stretch flex-lg-self-auto mr-0 mr-lg-3 mb-3 mb-lg-0 scoped-search site-scoped-search js-site-search position-relative "
+  role="search"
+>
+  <div class="position-relative">
+    <!-- '"` --><!-- </textarea></xmp> --></option></form><form class="js-site-search-form" role="search" aria-label="Site" data-scope-type="Repository" data-scope-id="212721722" data-scoped-search-url="/tanpattaya/tanflex.github.io/search" data-unscoped-search-url="/search" action="/tanpattaya/tanflex.github.io/search" accept-charset="UTF-8" method="get"><input name="utf8" type="hidden" value="&#x2713;" />
+      <label class="form-control input-sm header-search-wrapper p-0  js-chromeless-input-container">
+            <a class="header-search-scope no-underline" href="/tanpattaya/tanflex.github.io/blob/master/changes.atom">This repository</a>
+        <input type="text"
+          class="form-control input-sm header-search-input  js-site-search-focus js-site-search-field is-clearable"
+          data-hotkey="s,/"
+          name="q"
+          value=""
+          placeholder="Search"
+          data-unscoped-placeholder="Search GitHub"
+          data-scoped-placeholder="Search"
+          autocapitalize="off"
+          aria-label="Search this repository"
+          >
+          <input type="hidden" class="js-site-search-type-field" name="type" >
+   
 
 
 <script nonce="JWx8CR0X8cgt1xBXErcXvg==">
